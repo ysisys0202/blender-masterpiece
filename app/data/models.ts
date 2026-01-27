42983a053074a93021ddace3f2e2ae59b439d5e0
이윤슬 <ysisys0202@gmail.com>
2026-01-27 22:51:05 +0900

    feat: day2 작품 추가

--- app/data/models.ts
+++ app/data/models.ts
@@ -3,5 +3,10 @@ export const models = [
     image: "/images/postit.png",
     title: "Day 1. Post-it",
     model: "/models/postit.glb"
+  },
+  {
+    image: "/images/ruler.png",
+    title: "Day 2. Ruler",
+    model: "/models/ruler.glb"
   }
 ];
